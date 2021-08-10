081b39b13a7d3febaa9084bfd4a21f70cb8bc989
shl422 <h.k.domingo@outlook.com>
2021-08-10 09:53:17 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = '<h1>victory</h1>';
+newHeader.innerHTML = '<h1>victory</> <p1>SHAD is the champion</p1>';
+newHeader.id = 'victory';
